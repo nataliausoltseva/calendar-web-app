5679d1501ac90ba54fc495432b20c3b33a82dfe3
Natalia Usoltseva <usolnatalia@gmail.com>
2018-11-22 22:48:42 +1300

    Some changes

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -11,6 +11,19 @@ export default class EventsList extends React.Component {
                     <th scope="col">Location</th>
                     </tr>
                 </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">2018-11-22T08:58:48.42 - 2018-11-22T09:58:48.42</th>  
+                        <td>Birthday Party</td>
+                        <td>Auckland CBD</td>                      
+                    </tr>
+                    <tr>
+                        <th scope="row">2018-11-23T08:58:48.42 - 2018-11-23T10:58:48.42</th>
+                        <td>Job Interview</td>
+                        <td>OGGB Lab 5</td>
+                    </tr>
+
+                </tbody>
             </table>
         )
     }
