eea1d0dfe8b27c5b861fe59f6ec8ab132c4e9dc5
Natalia Usoltseva <usolnatalia@gmail.com>
2018-11-22 23:13:27 +1300

    Updates on the table

---
+++ src/Event.ts
@@ -0,0 +1,7 @@
+export interface IEvent {
+    id: number,
+    event: string,
+    location: string,
+    start: string,
+    end: string
+}
--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -1,30 +1,49 @@
 import * as React from "react";
+import { IEvent } from "../Event";
 
-export default class EventsList extends React.Component {
-    public render() {
-        return(
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                    <th scope="col">Date</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">2018-11-22T08:58:48.42 - 2018-11-22T09:58:48.42</th>  
-                        <td>Birthday Party</td>
-                        <td>Auckland CBD</td>                      
-                    </tr>
-                    <tr>
-                        <th scope="row">2018-11-23T08:58:48.42 - 2018-11-23T10:58:48.42</th>
-                        <td>Job Interview</td>
-                        <td>OGGB Lab 5</td>
-                    </tr>
+interface IProps {
+  events?: IEvent[];
+}
 
-                </tbody>
-            </table>
-        )
-    }
+export default class EventsList extends React.Component<IProps> {
+  public render() {
+    let { events } = this.props;
+    events = [];
+    events.push({
+      end: "2018-11-22T08:58:48.42",
+      event: "string",
+      id: 1,
+      location: "string",
+      start: "2018-11-22T08:58:48.42"
+    });
+
+    events.push({
+      end: "2018-11-22T10:51:13.687",
+      event: "string",
+      id: 6,
+      location: "string",
+      start: "2018-11-22T09:51:13.687"
+    });
+    return (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Name</th>
+            <th scope="col">Location</th>
+          </tr>
+        </thead>
+        <tbody>
+            {events.map(x=>{
+                return (
+                    <tr key = {x.id}>
+                    <td>{x.start + " - " + x.end}}</td>
+                    <td>{x.event}</td>
+                    <td>{x.location}</td>
+                    </tr>
+                    )})}
+        </tbody>
+      </table>
+    );
+  }
 }
