7644c0cafad87ec381dd75ac36e03d4f7cd82bcb
Natalia Usoltseva <usolnatalia@gmail.com>
2018-11-23 00:38:54 +1300

    Fixed calendar resizing for different types of phones

--- src/components/NovemberCalendar.tsx
+++ src/components/NovemberCalendar.tsx
@@ -7,18 +7,28 @@ export default class CalanderView extends React.Component {
     return (
       <div className="calander-view">
         <div className="row calander-view__headers">
-          <div className="col">Sunday</div>
-          <div className="col">Monday</div>
-          <div className="col">Tuesday</div>
-          <div className="col">Wednesday</div>
-          <div className="col">Thursday</div>
-          <div className="col">Friday</div>
-          <div className="col">Saturday</div>
+        <div className="d-none d-lg-block col">Sunday</div>
+          <div className="d-none d-lg-block col">Monday</div>
+          <div className="d-none d-lg-block col">Tuesday</div>
+          <div className="d-none d-lg-block col">Wednesday</div>
+          <div className="d-none d-lg-block col">Thursday</div>
+          <div className="d-none d-lg-block col">Friday</div>
+          <div className="d-none d-lg-block col">Saturday</div>
+
+          <div className="col d-lg-none">Sun</div>
+          <div className="col d-lg-none">Mon</div>
+          <div className="col d-lg-none">Tue</div>
+          <div className="col d-lg-none">Wed</div>
+          <div className="col d-lg-none">Thu</div>
+          <div className="col d-lg-none">Fri</div>
+          <div className="col d-lg-none">Sat</div>
         </div>
         {generatedOuput}
       </div>
     );
   }
+
+
   private generateCalanderForThisMonth(selectedDate: Date): any {
     const y = selectedDate.getFullYear();
     const m = selectedDate.getMonth();
@@ -37,7 +47,7 @@ export default class CalanderView extends React.Component {
       for (let day = 0; day < 7; day++) {
         const monthStyle = displayDate.getMonth() === m ? "calander-view__current-day" : "";
         days.push(
-          <div className={"col calander-view__day " + monthStyle}>{displayDate.getDate()}</div>
+          <div className={"col calander-view__day " + monthStyle} onClick={Date}>{displayDate.getDate()}</div>
         );
         displayDate.setDate(displayDate.getDate() + 1);
       }
