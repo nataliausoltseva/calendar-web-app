1bbba0788c6b0e2a329dd4e8d913d3178fda14ab
Natalia Usoltseva <usolnatalia@gmail.com>
2018-11-23 23:59:32 +1300

    New version

--- src/App.tsx
+++ src/App.tsx
@@ -7,282 +7,282 @@ import EventList from "./components/EventsList";
 import { IEvent } from "./Event";
 
 interface IState {
-  currentEvent: IEvent;
-  events: IEvent[];
-  open: boolean;
-  selectedDate: Date;
+	currentEvent: IEvent;
+	events: IEvent[];
+	open: boolean;
+	selectedDate: Date;
 }
 
 class App extends React.Component<{}, IState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      currentEvent: { id: 0, event: "", location: "", start: "", end: "" },
-      events: [],
-      open: false,
-      selectedDate: new Date(new Date().setHours(0, 0, 0, 0))
-    };
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			currentEvent: { id: 0, event: "", location: "", start: "", end: "" },
+			events: [],
+			open: false,
+			selectedDate: new Date(new Date().setHours(0, 0, 0, 0))
+		};
 
-    this.fetchMemes();
-    this.fetchMemes = this.fetchMemes.bind(this);
-    this.uploadEvent = this.uploadEvent.bind(this);
-  }
+		this.fetchMemes();
+		this.fetchMemes = this.fetchMemes.bind(this);
+		this.uploadEvent = this.uploadEvent.bind(this);
+	}
 
-  public render() {
-    const { open, selectedDate } = this.state;
-    const title =
-      moment(selectedDate).format("MMMM ") +
-      moment(selectedDate).format("YYYY") +
-      " Events";
-    return (
-      <div>
-        <div className="header-wrapper">
-          <div className="container header">
-            <img height="40" />
-            &nbsp; {title} &nbsp;
+	public render() {
+		const { open, selectedDate } = this.state;
+		const title =
+			moment(selectedDate).format("MMMM ") +
+			moment(selectedDate).format("YYYY") +
+			" Events";
+		return (
+			<div>
+				<div className="header-wrapper">
+					<div className="container header">
+						<img height="40" />
+						&nbsp; {title} &nbsp;
           </div>
-        </div>
-        <div className="container">
-          <CalendarView
-            selectedDate={this.state.selectedDate}
-            updateSelectedDate={this.updateSelectedDate}
-          />
-          <EventList
-            events={this.state.events}
-            selectedDate={this.state.selectedDate}
-            onEditClicked={this.onEditClicked}
-          />
-          <div
-            className="btn btn-primary btn-action btn-add"
-            onClick={this.onOpenModal}
-          >
-            Add Event
+				</div>
+				<div className="container">
+					<CalendarView
+						selectedDate={this.state.selectedDate}
+						updateSelectedDate={this.updateSelectedDate}
+					/>
+					<EventList
+						events={this.state.events}
+						selectedDate={this.state.selectedDate}
+						onEditClicked={this.onEditClicked}
+					/>
+					<div
+						className="btn btn-primary btn-action btn-add"
+						onClick={this.onOpenModal}
+					>
+						Add Event
           </div>
-        </div>
+				</div>
 
-        <Modal open={open} onClose={this.onCloseModal}>
-          <form>
-            <div className="form-group">
-              <label>Event Name</label>
-              <input
-                defaultValue={this.state.currentEvent.event}
-                type="text"
-                className="form-control"
-                id="november-event-input"
-                placeholder="Enter Event"
-              />
-              <small className="form-text text-muted">
-                You can edit any event later
+				<Modal open={open} onClose={this.onCloseModal}>
+					<form>
+						<div className="form-group">
+							<label>Event Name</label>
+							<input
+								defaultValue={this.state.currentEvent.event}
+								type="text"
+								className="form-control"
+								id="november-event-input"
+								placeholder="Enter Event"
+							/>
+							<small className="form-text text-muted">
+								You can edit any event later
               </small>
-            </div>
-            <div className="form-group">
-              <label>Event Location</label>
-              <input
-      defaultValue={this.state.currentEvent.location}
-                type="text"
-                className="form-control"
-                id="november-location-input"
-                placeholder="Enter Event Location"
-              />
-              <small className="form-text text-muted">
-                You can edit any event location later
+						</div>
+						<div className="form-group">
+							<label>Event Location</label>
+							<input
+								defaultValue={this.state.currentEvent.location}
+								type="text"
+								className="form-control"
+								id="november-location-input"
+								placeholder="Enter Event Location"
+							/>
+							<small className="form-text text-muted">
+								You can edit any event location later
               </small>
-            </div>
-            <div className="form-group">
-              <label>Event Start Time</label>
-              <input
-      defaultValue={moment(this.state.currentEvent.start).format('HH:mm')}
-                type="text"
-                className="form-control"
-                id="november-event-start-input"
-                placeholder="Enter Event Start Time"
-              />
-              <small className="form-text text-muted">Format HH:MM</small>
-            </div>
-            <div className="form-group">
-              <label>Event End Time</label>
-              <input
-      defaultValue={moment(this.state.currentEvent.end).format('HH:mm')}
-                type="text"
-                className="form-control"
-                id="november-event-end-input"
-                placeholder="Enter Event End Time"
-              />
-              <small className="form-text text-muted">Format HH:MM</small>
-            </div>
-            <button type="button" className="btn" onClick={this.uploadEvent}>
-              Save
+						</div>
+						<div className="form-group">
+							<label>Event Start Time</label>
+							<input
+								defaultValue={moment(this.state.currentEvent.start).format('HH:mm')}
+								type="text"
+								className="form-control"
+								id="november-event-start-input"
+								placeholder="Enter Event Start Time"
+							/>
+							<small className="form-text text-muted">Format HH:MM</small>
+						</div>
+						<div className="form-group">
+							<label>Event End Time</label>
+							<input
+								defaultValue={moment(this.state.currentEvent.end).format('HH:mm')}
+								type="text"
+								className="form-control"
+								id="november-event-end-input"
+								placeholder="Enter Event End Time"
+							/>
+							<small className="form-text text-muted">Format HH:MM</small>
+						</div>
+						<button type="button" className="btn" onClick={this.uploadEvent}>
+							Save
             </button>
-          </form>
-        </Modal>
-      </div>
-    );
-  }
+					</form>
+				</Modal>
+			</div>
+		);
+	}
 
-  private updateSelectedDate = (selectedDate: Date): void => {
-    this.setState({ selectedDate });
-  };
+	private updateSelectedDate = (selectedDate: Date): void => {
+		this.setState({ selectedDate });
+	};
 
-  // Modal open
-  private onOpenModal = () => {
-    this.setState({ open: true });
-  };
+	// Modal open
+	private onOpenModal = () => {
+		this.setState({ open: true });
+	};
 
-  // Modal close
-  private onCloseModal = () => {
-    this.setState({ open: false });
-  };
+	// Modal close
+	private onCloseModal = () => {
+		this.setState({ open: false });
+	};
 
-  private onEditClicked = (event: IEvent) => (
-    mouseEvent: React.MouseEvent<HTMLElement>
-  ) => {
-    this.setState({ currentEvent: event });
-    this.onOpenModal();
-  };
+	private onEditClicked = (event: IEvent) => (
+		mouseEvent: React.MouseEvent<HTMLElement>
+	) => {
+		this.setState({ currentEvent: event });
+		this.onOpenModal();
+	};
 
-  // GET memes
-  private fetchMemes() {
-    const url = "https://nucalendarapi.azurewebsites.net/api/Calendar";
-    fetch(url, {
-      method: "GET"
-    })
-      .then(res => res.json())
-      .then(data => {
-        const events: IEvent[] = data;
-        this.setState({
-          events
-        });
-      });
-  }
+	// GET memes
+	private fetchMemes() {
+		const url = "https://nucalendarapi.azurewebsites.net/api/Calendar";
+		fetch(url, {
+			method: "GET"
+		})
+			.then(res => res.json())
+			.then(data => {
+				const events: IEvent[] = data;
+				this.setState({
+					events
+				});
+			});
+	}
 
-  // POST meme
-  private uploadEvent() {
-    const eventInput = document.getElementById(
-      "november-event-input"
-    ) as HTMLInputElement;
-    const locationInput = document.getElementById(
-      "november-location-input"
-    ) as HTMLInputElement;
-    const startInput = document.getElementById(
-      "november-event-start-input"
-    ) as HTMLInputElement;
-    const endInput = document.getElementById(
-      "november-event-end-input"
-    ) as HTMLInputElement;
-    if (
-      eventInput === null ||
-      locationInput === null ||
-      startInput === null ||
-      endInput === null
-    ) {
-      return;
-    }
+	// POST meme
+	private uploadEvent() {
+		const eventInput = document.getElementById(
+			"november-event-input"
+		) as HTMLInputElement;
+		const locationInput = document.getElementById(
+			"november-location-input"
+		) as HTMLInputElement;
+		const startInput = document.getElementById(
+			"november-event-start-input"
+		) as HTMLInputElement;
+		const endInput = document.getElementById(
+			"november-event-end-input"
+		) as HTMLInputElement;
+		if (
+			eventInput === null ||
+			locationInput === null ||
+			startInput === null ||
+			endInput === null
+		) {
+			return;
+		}
 
-    // Validate time
-    const startTimeParts = startInput.value.split(":");
-    if (startTimeParts.length !== 2) {
-      alert("Please specify start time in correct format.");
-      return;
-    }
+		// Validate time
+		const startTimeParts = startInput.value.split(":");
+		if (startTimeParts.length !== 2) {
+			alert("Please specify start time in correct format.");
+			return;
+		}
 
-    const startHours = Number(startTimeParts[0]);
-    const startMinutes = Number(startTimeParts[1]);
-    if (
-      isNaN(startHours) ||
-      isNaN(startMinutes) ||
-      startHours < 0 ||
-      startHours > 23 ||
-      startMinutes < 0 ||
-      startMinutes > 59
-    ) {
-      alert("Please specify start time in correct format.");
-      return;
-    }
+		const startHours = Number(startTimeParts[0]);
+		const startMinutes = Number(startTimeParts[1]);
+		if (
+			isNaN(startHours) ||
+			isNaN(startMinutes) ||
+			startHours < 0 ||
+			startHours > 23 ||
+			startMinutes < 0 ||
+			startMinutes > 59
+		) {
+			alert("Please specify start time in correct format.");
+			return;
+		}
 
-    const endTimeParts = endInput.value.split(":");
-    if (endTimeParts.length !== 2) {
-      alert("Please specify end time in correct format.");
-      return;
-    }
+		const endTimeParts = endInput.value.split(":");
+		if (endTimeParts.length !== 2) {
+			alert("Please specify end time in correct format.");
+			return;
+		}
 
-    const endHours = Number(endTimeParts[0]);
-    const endMinutes = Number(endTimeParts[1]);
-    if (
-      isNaN(endHours) ||
-      isNaN(endMinutes) ||
-      endHours < 0 ||
-      endHours > 23 ||
-      endMinutes < 0 ||
-      endMinutes > 59
-    ) {
-      alert("Please specify end time in correct format.");
-      return;
-    }
+		const endHours = Number(endTimeParts[0]);
+		const endMinutes = Number(endTimeParts[1]);
+		if (
+			isNaN(endHours) ||
+			isNaN(endMinutes) ||
+			endHours < 0 ||
+			endHours > 23 ||
+			endMinutes < 0 ||
+			endMinutes > 59
+		) {
+			alert("Please specify end time in correct format.");
+			return;
+		}
 
-    const event = eventInput.value;
-    const locationEvent = locationInput.value;
-    const start = moment(this.state.selectedDate).toDate();
-    start.setHours(startHours);
-    start.setMinutes(startMinutes);
-    const end = moment(this.state.selectedDate).toDate();
-    end.setHours(endHours);
-    end.setMinutes(endMinutes);
+		const event = eventInput.value;
+		const locationEvent = locationInput.value;
+		const start = moment(this.state.selectedDate).toDate();
+		start.setHours(startHours);
+		start.setMinutes(startMinutes);
+		const end = moment(this.state.selectedDate).toDate();
+		end.setHours(endHours);
+		end.setMinutes(endMinutes);
 
-    if (start.getTime() > end.getTime()) {
-      alert("End date cannot be before start date.");
-      return;
-    }
+		if (start.getTime() > end.getTime()) {
+			alert("End date cannot be before start date.");
+			return;
+		}
 
-    const eventJson: string = JSON.stringify({
-      end: end.toISOString(),
-      event,
-      id: this.state.currentEvent.id,
-      location: locationEvent,
-      start: start.toISOString()
-    });
+		const eventJson: string = JSON.stringify({
+			end: end.toISOString(),
+			event,
+			id: this.state.currentEvent.id,
+			location: locationEvent,
+			start: start.toISOString()
+		});
 
-    if (this.state.currentEvent.id === 0) {
-      // Create
-      const url = "https://nucalendarapi.azurewebsites.net/api/Calendar";
+		if (this.state.currentEvent.id === 0) {
+			// Create
+			const url = "https://nucalendarapi.azurewebsites.net/api/Calendar";
 
-      fetch(url, {
-        body: eventJson,
-        headers: {
-          "Content-Type": "application/json",
-          "cache-control": "no-cache"
-        },
-        method: "POST"
-      }).then((response: any) => {
-        if (!response.ok) {
-          // Error State
-          alert(response.statusText);
-        } else {
-          location.reload();
-        }
-      });
-    } else {
-      // Update
-      const url =
-        "https://nucalendarapi.azurewebsites.net/api/Calendar/" +
-        this.state.currentEvent.id;
+			fetch(url, {
+				body: eventJson,
+				headers: {
+					"Content-Type": "application/json",
+					"cache-control": "no-cache"
+				},
+				method: "POST"
+			}).then((response: any) => {
+				if (!response.ok) {
+					// Error State
+					alert(response.statusText);
+				} else {
+					location.reload();
+				}
+			});
+		} else {
+			// Update
+			const url =
+				"https://nucalendarapi.azurewebsites.net/api/Calendar/" +
+				this.state.currentEvent.id;
 
-      fetch(url, {
-        body: eventJson,
-        headers: {
-          "Content-Type": "application/json",
-          "cache-control": "no-cache"
-        },
-        method: "PUT"
-      }).then((response: any) => {
-        if (!response.ok) {
-          // Error State
-          alert(response.statusText);
-        } else {
-          location.reload();
-        }
-      });
-    }
-  }
+			fetch(url, {
+				body: eventJson,
+				headers: {
+					"Content-Type": "application/json",
+					"cache-control": "no-cache"
+				},
+				method: "PUT"
+			}).then((response: any) => {
+				if (!response.ok) {
+					// Error State
+					alert(response.statusText);
+				} else {
+					location.reload();
+				}
+			});
+		}
+	}
 }
 
 export default App;
--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -3,79 +3,79 @@ import * as React from "react";
 import { IEvent } from "../Event";
 
 interface IProps {
-  events: IEvent[];
-  selectedDate: Date;
-  onEditClicked: (event: IEvent) => (
-    event: React.MouseEvent<HTMLElement>
-  )=> void;
+    events: IEvent[];
+    selectedDate: Date;
+    onEditClicked: (event: IEvent) => (
+        event: React.MouseEvent<HTMLElement>
+    ) => void;
 }
 
 export default class EventsList extends React.Component<IProps> {
-  public render() {
-    const { events, selectedDate } = this.props;
+    public render() {
+        const { events, selectedDate } = this.props;
 
-    return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Date</th>
-            <th scope="col">Name</th>
-            <th scope="col">Location</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {events
-            .filter(
-              x =>
-                moment(x.start).format("YYYY-MM-DD") ===
-                moment(selectedDate).format("YYYY-MM-DD")
-            )
-            .map(x => {
-              return (
-                <tr key={x.id}>
-                  <td>
-                    {moment(x.start).format("HH:mm") +
-                      " - " +
-                      moment(x.end).format("HH:mm")}
-                  </td>
-                  <td>{x.event}</td>
-                  <td>{x.location}</td>
-                  <td
-                    className="btn btn-primary btn-action"
-                    onClick={this.props.onEditClicked(x)}
-                  >
-                    Edit{" "}
-                  </td>
-                  <td
-                    className="btn btn-primary btn-action"
-                    onClick={this.deleteEvent(x.id)}
-                  >
-                    Delete{" "}
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
-    );
-  }
+        return (
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Date</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {events
+                        .filter(
+                            x =>
+                                moment(x.start).format("YYYY-MM-DD") ===
+                                moment(selectedDate).format("YYYY-MM-DD")
+                        )
+                        .map(x => {
+                            return (
+                                <tr key={x.id}>
+                                    <td>
+                                        {moment(x.start).format("HH:mm") +
+                                            " - " +
+                                            moment(x.end).format("HH:mm")}
+                                    </td>
+                                    <td>{x.event}</td>
+                                    <td>{x.location}</td>
+                                    <td
+                                        className="btn btn-primary btn-action"
+                                        onClick={this.props.onEditClicked(x)}
+                                    >
+                                        Edit{" "}
+                                    </td>
+                                    <td
+                                        className="btn btn-primary btn-action"
+                                        onClick={this.deleteEvent(x.id)}
+                                    >
+                                        Delete{" "}
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                </tbody>
+            </table>
+        );
+    }
 
-  // DELETE event
-  private deleteEvent = (id: number) => (
-    event: React.MouseEvent<HTMLElement>
-  ) => {
-    const url = "https://nucalendarapi.azurewebsites.net/api/Calendar/" + id;
+    // DELETE event
+    private deleteEvent = (id: number) => (
+        event: React.MouseEvent<HTMLElement>
+    ) => {
+        const url = "https://nucalendarapi.azurewebsites.net/api/Calendar/" + id;
 
-    fetch(url, {
-      method: "DELETE"
-    }).then((response: any) => {
-      if (!response.ok) {
-        // Error Response
-        alert(response.statusText);
-      } else {
-        location.reload();
-      }
-    });
-  };
+        fetch(url, {
+            method: "DELETE"
+        }).then((response: any) => {
+            if (!response.ok) {
+                // Error Response
+                alert(response.statusText);
+            } else {
+                location.reload();
+            }
+        });
+    };
 }
